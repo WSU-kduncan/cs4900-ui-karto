4a07b902ffdb9f261e5b440e58450733f24c433f
Joshua Wise <joshua@Joshuas-MacBook-Air.local>
2025-11-06 11:16:09 -0500

    Car-list added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarList } from './car-list/car-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarList],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('karto-ui');
