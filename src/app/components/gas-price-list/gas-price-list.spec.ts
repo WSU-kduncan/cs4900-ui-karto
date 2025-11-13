405d74a0c6dabb5d02ec22fdb14975a3ea18f79f
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-11-13 13:12:06 -0600

    Add GasPrice detail, list, and service

---
+++ src/app/components/gas-price-list/gas-price-list.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GasPriceList } from './gas-price-list';
+
+describe('GasPriceList', () => {
+  let component: GasPriceList;
+  let fixture: ComponentFixture<GasPriceList>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GasPriceList]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GasPriceList);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
