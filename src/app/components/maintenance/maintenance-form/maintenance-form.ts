cc0e0dea9c2cd8f9853f227f77638f1c3700d2d0
Owen <kemp.56@wright.edu>
2025-11-18 14:55:28 -0500

    Create Maintenance Form

---
+++ src/app/components/maintenance/maintenance-form/maintenance-form.ts
@@ -0,0 +1,58 @@
+import { formatDate } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MaintenanceService } from '@services/maintenance.service';
+import { MaintenanceDto } from '@shared/models/dtos.interface';
+import { Button } from "primeng/button";
+
+@Component({
+    selector: 'app-maintenance-form',
+    imports: [ReactiveFormsModule, Button],
+    templateUrl: './maintenance-form.html',
+    styleUrl: './maintenance-form.scss',
+})
+export class MaintenanceForm {
+    private readonly maintenanceService = inject(MaintenanceService);
+    formBuilder = inject(FormBuilder);
+    postMaintenanceError = signal('');
+    form = this.formBuilder.group(
+        {
+            cost: [0, Validators.required],
+            date: [formatDate(new Date(), 'yyyy-MM-ddTHH:mm', 'en-US'), Validators.required],
+            mileage: [0, Validators.required]
+        }
+    )
+
+    get cost() {
+        return this.form.get('cost')!;
+    }
+
+    get date() {
+        return this.form.get('date')!;
+    }
+
+    get mileage() {
+        return this.form.get('mileage')!;
+    }
+
+    onSubmit() {
+        const dto: MaintenanceDto = {
+            carVin: '1HGCM82633A004352',
+            cost: this.cost.value!,
+            date: new Date(this.date.value!).toISOString(),
+            id: 0,
+            itemDetails: [],
+            mileage: this.mileage.value!,
+            receipt: null,
+        };
+
+        console.log(dto)
+
+        this.maintenanceService.postMaintenance(dto).subscribe({
+            error: (err: HttpErrorResponse) => {
+                this.postMaintenanceError.set(err.error ?? 'Could not create maintenance');
+            },
+        });
+    }
+}
--- src/app/components/maintenance/maintenance-list/maintenance-list.ts
+++ src/app/components/maintenance/maintenance-list/maintenance-list.ts
@@ -3,11 +3,11 @@ import { MaintenanceDto } from '@shared/models/dtos.interface';
 import { MaintenanceService } from '@services/maintenance.service';
 import { MaintenanceItem } from '@components/maintenance/maintenance-detail/maintenance-detail';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { HttpErrorResponse } from '@angular/common/http';
+import { MaintenanceForm } from "../maintenance-form/maintenance-form";
 
 @Component({
   selector: 'app-maintenance-list',
-  imports: [MaintenanceItem],
+  imports: [MaintenanceItem, MaintenanceForm],
   templateUrl: './maintenance-list.html',
   styleUrl: './maintenance-list.scss',
   standalone: true,
@@ -21,47 +21,4 @@ export class MaintenanceList {
       initialValue: [],
     }
   );
-  postMaintenancerError = signal('');
-  maintenanceId = signal(0);
-  date = signal(new Date().toLocaleDateString());
-  cost = signal(0);
-  mileage = signal(0);
-
-  onMaintenanceIdChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.maintenanceId.set(parseInt(input.value));
-  }
-
-  onCostChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.cost.set(parseFloat(input.value));
-  }
-
-  onDateChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.date.set(input.value as any);
-  }
-
-  onMileageChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.mileage.set(parseInt(input.value));
-  }
-
-  addMaintenanceId() {
-    const dto: MaintenanceDto = {
-      carVin: '1HGCM82633A004352',
-      cost: this.cost(),
-      date: new Date(this.date()).toISOString(),
-      id: this.maintenanceId(),
-      itemDetails: [],
-      mileage: this.mileage(),
-      receipt: null,
-    };
-
-    this.maintenanceService.postMaintenance(dto).subscribe({
-      error: (err: HttpErrorResponse) => {
-        this.postMaintenancerError.set(err.error ?? 'Could not create maintenance');
-      },
-    });
-  }
 }
