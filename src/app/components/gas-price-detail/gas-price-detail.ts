08869dc3eb0ba2e0c1ed254654f5d43079d628f5
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-11-20 20:47:10 -0600

    Make gas-price-detail components more readable on the website

--- src/app/components/gas-price-detail/gas-price-detail.ts
+++ src/app/components/gas-price-detail/gas-price-detail.ts
@@ -1,14 +1,17 @@
-import { Component, input } from '@angular/core';
+import {Component, input, LOCALE_ID} from '@angular/core';
 import { GasPriceDto } from '@shared/models/dtos.interface';
 import { PanelModule } from 'primeng/panel';
+import {CurrencyPipe, formatDate} from '@angular/common';
+import {Tab} from 'primeng/tabs';
 
 @Component({
   selector: 'app-gas-price-detail',
   standalone: true,
-  imports: [PanelModule],
+  imports: [PanelModule, CurrencyPipe],
   templateUrl: './gas-price-detail.html',
   styleUrl: './gas-price-detail.scss',
 })
 export class GasPriceDetail {
   public price = input.required<GasPriceDto>();
+  protected readonly formatDate = formatDate;
 }
