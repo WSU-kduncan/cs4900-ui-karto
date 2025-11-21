f4f74b6e2285c08c7507582ae17e6dda46c47d5f
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-11-20 20:49:02 -0600

    Remove old signal-based gas-price creation in favor of form & add counter state

--- src/app/components/gas-price-list/gas-price-list.ts
+++ src/app/components/gas-price-list/gas-price-list.ts
@@ -4,18 +4,21 @@ import { GasPriceDetail } from '../gas-price-detail/gas-price-detail';
 
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import {toSignal} from '@angular/core/rxjs-interop';
-import {GasPriceDto} from '@shared/models/dtos.interface';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { GasPriceDto } from '@shared/models/dtos.interface';
+import {GasPriceForm} from '@components/gas-price-form/gas-price-form';
 
 @Component({
   selector: 'app-gas-price-list',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, GasPriceDetail],
+  imports: [ButtonModule, InputTextModule, GasPriceDetail, GasPriceForm],
   templateUrl: './gas-price-list.html',
   styleUrl: './gas-price-list.scss',
 })
 export class GasPriceList {
   readonly #gasService = inject(GasService);
+
+  public currentGasStationId: number = -1;
 
   public gasPrices: Signal<GasPriceDto[]> = toSignal(
     this.#gasService.getGasPriceList(),
@@ -24,38 +27,7 @@ export class GasPriceList {
     }
   );
 
-  newGasPriceValue = signal(0);
-  newGasStationIDValue = signal(0);
-  newGasTypeIDValue = signal(0);
-
-  whenNewGasPriceGiven(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.newGasPriceValue.set(Number(input.value));
-  }
-
-  whenNewGasStationIDGiven(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.newGasStationIDValue.set(Number(input.value));
-  }
-
-  whenNewGasTypeIDGiven(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.newGasTypeIDValue.set(Number(input.value));
-  }
-
-  addGasPrice() {
-    if (this.newGasPriceValue() && this.newGasStationIDValue() && this.newGasTypeIDValue()) {
-      const gasPriceDto: GasPriceDto = {
-        id: {
-          gasStationId: this.newGasStationIDValue(),
-          gasTypeId: this.newGasTypeIDValue()
-        },
-        price: this.newGasPriceValue(),
-        updated: new Date()
-      }
-
-      // add/post the new gas price
-      this.#gasService.addGasPrice(gasPriceDto).subscribe();
-    }
+  public updateTrackedGasStationId(newId: number ) {
+    this.currentGasStationId = newId;
   }
 }
