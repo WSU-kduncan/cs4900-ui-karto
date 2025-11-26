3e5f8234abeb8918b09fa92999b9e53b033e1937
Owen <kemp.56@wright.edu>
2025-11-25 20:22:17 -0500

    Maintenance Receipt Button

--- src/app/components/maintenance/maintenance-detail/maintenance-detail.ts
+++ src/app/components/maintenance/maintenance-detail/maintenance-detail.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, output } from '@angular/core';
+import { Component, inject, input, OnDestroy, OnInit, output, signal } from '@angular/core';
 import { MaintenanceDto } from '@shared/models/dtos.interface';
 import { CurrencyPipe, DatePipe, DecimalPipe } from '@angular/common';
 import { Button } from 'primeng/button';
@@ -11,7 +11,7 @@ import { MaintenanceService } from '@services/maintenance.service';
   templateUrl: './maintenance-detail.html',
   styleUrl: './maintenance-detail.scss',
 })
-export class MaintenanceItem {
+export class MaintenanceItem implements OnDestroy, OnInit {
   maintenance = input.required<MaintenanceDto>();
   maintenanceService = inject(MaintenanceService);
   onDelete = output();
@@ -22,5 +22,25 @@ export class MaintenanceItem {
         this.onDelete.emit();
       },
     });
+  }
+
+  imageUrl = signal<string | null>(null);
+
+  ngOnInit(): void {
+    if (this.maintenance().receipt) this.setImage(this.maintenance().receipt!);
+  }
+
+  openReceipt() {
+    window.open(this.imageUrl()!, '_blank');
+  }
+
+  setImage(bytes: Uint8Array, type = 'image/png') {
+    if (this.imageUrl()) URL.revokeObjectURL(this.imageUrl()!);
+    const blob = new Blob([bytes.slice()], { type });
+    this.imageUrl.set(URL.createObjectURL(blob));
+  }
+
+  ngOnDestroy() {
+    if (this.imageUrl()) URL.revokeObjectURL(this.imageUrl()!);
   }
 }
